ad04029726238018d2f532310f7b9c64cd414162
Luigi Freitas Cruz <luigifcruz@icloud.com>
2016-06-10 21:25:14 -0300

    First Commit

---
+++ server/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../stylesheet/App.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div>This is a react App.</div>
+    )
+  }
+}
+
+export default App
---
+++ server/server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path    = require('path');
+const config = require('../webpack.config.js');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+var app = express();
+
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
+
+app.use(express.static('./dist'));
+
+app.use('*', function (req, res) {
+  res.sendFile(path.resolve('./client/index.html'));
+});
+
+var port = 5000;
+
+app.listen(port, function(error) {
+  if (error) throw error;
+  console.log("Express server listening on port", port);
+});
